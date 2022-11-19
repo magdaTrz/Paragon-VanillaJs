d7b52e9e522604cbfc0ecb94629531a111424ac4
magdaTrzTrash <mwt.trash@gmail.com>
2022-11-19 19:50:34 +0100

    działa automatyczne dodawanie produktów z localStorage i wypisuje je na paragon

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//id 
-idNumer = 2;
+//id
+idNumer = 1;
 
 //Selectors
 //adding new Product
@@ -10,78 +10,160 @@ const newProductButton = document.querySelector('.newProduct-button');
 const receiptList = document.querySelector(".receipt-list");
 
 //Event Listeners
+document.addEventListener("DOMContentLoaded", getProducts);
 newProductButton.addEventListener('click', addProduct);
 receiptList.addEventListener('click', deleteEditProduct);
 
 //Functions
-function addProduct(event){
-    event.preventDefault(); //prevent form from submitting
+function addProduct(event) {
+  console.log('działa')
+  event.preventDefault(); //prevent form from submitting
 
-    //div
-    const productDiv = document.createElement('tr'); //toodoDiv
-    productDiv.classList.add("product"); //toodo
-    //create td's
-    const idTd = document.createElement('td'); //newTodo
-    const descriptionTd = document.createElement('td');
-    const quantityTd = document.createElement('td');
-    const priceTd = document.createElement('td');
-    const totalTd = document.createElement('td');
+  //div
+  const productDiv = document.createElement('tr'); //toodoDiv
+  productDiv.classList.add("product"); //toodo
+  //create td's
+  const idTd = document.createElement('td'); //newTodo
+  const descriptionTd = document.createElement('td');
+  const quantityTd = document.createElement('td');
+  const priceTd = document.createElement('td');
+  const totalTd = document.createElement('td');
 
-    idTd.innerText = idNumer;
-    descriptionTd.innerText = newDescriptionInput.value;
-    quantityTd.innerText = newQuantityInput.value;
-    priceTd.innerText = newPriceInput.value;
-    const total = (newQuantityInput.value) * (newPriceInput.value)
-    totalTd.innerText = total
+  idTd.innerText = idNumer;
+  descriptionTd.innerText = newDescriptionInput.value;
+  quantityTd.innerText = newQuantityInput.value;
+  priceTd.innerText = newPriceInput.value;
+  const total = (newQuantityInput.value) * (newPriceInput.value)
+  totalTd.innerText = total
 
-    idTd.classList.add("product-item"); //toodo-item
-    descriptionTd.classList.add("product-item");
-    quantityTd.classList.add("product-item");
-    priceTd.classList.add("product-item");
-    totalTd.classList.add("product-item");
+  idTd.classList.add("product-item"); //toodo-item
+  descriptionTd.classList.add("product-item");
+  quantityTd.classList.add("product-item");
+  priceTd.classList.add("product-item");
+  totalTd.classList.add("product-item");
 
-    productDiv.appendChild(idTd);
-    productDiv.appendChild(descriptionTd);
-    productDiv.appendChild(quantityTd);
-    productDiv.appendChild(priceTd);
-    productDiv.appendChild(totalTd);
+  productDiv.appendChild(idTd);
+  productDiv.appendChild(descriptionTd);
+  productDiv.appendChild(quantityTd);
+  productDiv.appendChild(priceTd);
+  productDiv.appendChild(totalTd);
 
-    //Delete product
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "<td>Delete </td>";
-    deleteButton.classList.add("delete-button");
-    productDiv.appendChild(deleteButton);
+  //add to localStorage
+  saveLocalReceipt(newDescriptionInput.value);
 
-    //Edit button
-    const editButton = document.createElement("button");
-    editButton.innerHTML = "<td>Edit</td>";
-    editButton.classList.add("edit-button");
-    productDiv.appendChild(editButton);
+  //Delete product
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "<td>Delete </td>";
+  deleteButton.classList.add("delete-button");
+  productDiv.appendChild(deleteButton);
 
-    //append to list
-    receiptList.appendChild(productDiv);
-    
-    //clear imputs
-    newDescriptionInput.value = "";
-    newPriceInput.value = "";
-    newQuantityInput.value = "";
-    //increase id number
-    idNumer++;
+  //Edit button
+  const editButton = document.createElement("button");
+  editButton.innerHTML = "<td>Edit</td>";
+  editButton.classList.add("edit-button");
+  productDiv.appendChild(editButton);
+
+  //append to list
+  receiptList.appendChild(productDiv);
+
+  //clear imputs
+  newDescriptionInput.value = "";
+  newPriceInput.value = "";
+  newQuantityInput.value = "";
+  //increase id number
+  idNumer++;
 
 }
 
 function deleteEditProduct(event) {
-    console.log(event.target);
-    event.target.style.backgroundColor = "red";
-    const item = event.target;
-    //delete product
-    if(item.classList[0] === "delete-button") {
-        const element = item.parentElement;
-        element.remove();
-    }
+  event.target.style.backgroundColor = "red";
+  const item = event.target;
+  //delete product
+  if (item.classList[0] === "delete-button") {
+    const element = item.parentElement;
+    element.remove();
+  }
 
-    //edit product
-    if(item.classList[0] === "edit-button") {
-        
-    }
+  //edit product
+  if (item.classList[0] === "edit-button") {
+
+  }
 }
+
+// function editProduct(e) {
+//     const product = receiptList.childNodes
+//     console.log(product.length)
+//     console.log(product);
+//     for(let i = 2; i < product.length){
+
+//     }
+// }
+
+function saveLocalReceipt(product, what) {
+  //check if some product already exists
+  let products;
+  if (localStorage.getItem('products') === null) {
+    products = [];
+  } else {
+    products = JSON.parse(localStorage.getItem('products'));
+  }
+  products.push(product);
+  localStorage.setItem('products', JSON.stringify(products));
+}
+
+function getProducts() {
+  let products;
+  //check if some product already exists
+  if (localStorage.getItem('products') === null) {
+    products = [];
+  } else {
+    products = JSON.parse(localStorage.getItem('products'));
+  }
+  products.forEach(function(product) {
+      //div
+      const productDiv = document.createElement('tr'); //toodoDiv
+      productDiv.classList.add("product"); //toodo
+      //create td's
+      const idTd = document.createElement('td'); //newTodo
+      const descriptionTd = document.createElement('td');
+      const quantityTd = document.createElement('td');
+      const priceTd = document.createElement('td');
+      const totalTd = document.createElement('td');
+
+      idTd.innerText = product;
+      // descriptionTd.innerText = newDescriptionInput.value;
+      // quantityTd.innerText = newQuantityInput.value;
+      // priceTd.innerText = newPriceInput.value;
+      // const total = (newQuantityInput.value) * (newPriceInput.value)
+      // totalTd.innerText = total
+
+      idTd.classList.add("product-item"); //toodo-item
+      descriptionTd.classList.add("product-item");
+      quantityTd.classList.add("product-item");
+      priceTd.classList.add("product-item");
+      totalTd.classList.add("product-item");
+
+      productDiv.appendChild(idTd);
+      productDiv.appendChild(descriptionTd);
+      productDiv.appendChild(quantityTd);
+      productDiv.appendChild(priceTd);
+      productDiv.appendChild(totalTd);
+
+      //Delete product
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "<td>Delete </td>";
+      deleteButton.classList.add("delete-button");
+      productDiv.appendChild(deleteButton);
+
+      //Edit button
+      const editButton = document.createElement("button");
+      editButton.innerHTML = "<td>Edit</td>";
+      editButton.classList.add("edit-button");
+      productDiv.appendChild(editButton);
+
+      //append to list
+      receiptList.appendChild(productDiv);
+    });
+  }
+
+  
